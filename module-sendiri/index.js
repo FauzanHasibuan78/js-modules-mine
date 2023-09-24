4b4b71474de3e0e376d0b5fc78f789f574fabb11
FauzanHasibuan78 <fauzanhasibuan78@gmail.com>
2023-09-25 00:16:00 +0700

    test user-senter-module

---
+++ module-sendiri/index.js
@@ -0,0 +1,16 @@
+function pangkatDua(angka) {
+    return angka * angka
+}
+
+function pangkatTiga(angka) {
+    return angla * angka * angka
+}
+
+function nama() {
+    return nama
+}
+
+// kalau mau export beberapa file dalam 
+// satu kali eksport, gunakan module.eksports = {module yang mau di eksport1 dan module eksport2}
+// ini penulisan common JS
+module.exports = { pangkatTiga, pangkatDua, nama}
---
+++ module-sendiri/main.js
@@ -0,0 +1,5 @@
+const { pangkatDua, pangkatTiga, nama} = require('./index')
+
+console.log(pangkatDua)
+console.log(pangkatTiga)
+console.log(nama)
